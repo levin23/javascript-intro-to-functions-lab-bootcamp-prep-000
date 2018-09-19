611178cdd02420a70281afca5e4de73617c693c8
Levi C Uzodike <leviuzodike@gmail.com>
2018-09-19 16:40:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,5 +27,5 @@ function sayHiToGrandma(string) {
   if(string.toLowerCase()==string)
     return "I can't hear you!"
 }
-
-console.log("hELLO"=="HELLO".toUpperCase())
+var word = "HELLO"
+console.log(word.toUpperCase()==word)
