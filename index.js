42a6bb7b060f67ac2552e6b164fe02a70a7090e9
Levi C Uzodike <leviuzodike@gmail.com>
2018-09-19 16:35:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ function whisper(string) {
 }
 
 function logShout(string) {
- console.log(string.toUppeCase())
+ console.log(string.toUpperCase())
 }
 
 function logWhisper(string) {
-  console.log( string.toUppeCase())
+  console.log( string.toUpperCase())
 }
 
 function sayHiToGrandma(string) {
-  if(string.toUppeCase==string)
+  if(string.toUpperCase()==string)
     return "YES INDEED!"
   else if(string=="I love you, Grandma.")
     return "I love you, too."
@@ -24,6 +24,6 @@ function sayHiToGrandma(string) {
 }
 
 function sayHiToGrandma(string) {
-  if(string.toLowerCase==string)
+  if(string.toLowerCase()==string)
     return "I can't hear you!"
 }
